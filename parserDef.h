f984558554aee5fe6fe69d7a216a82fb6f23632d
Raghav Kasyap <raghavk2503@gmail.com>
2022-03-01 20:51:40 +0530

    Complete definitions till first and follow sets

--- parserDef.h
+++ parserDef.h
@@ -4,58 +4,62 @@
 
 #include "lexerDef.h"
 
-#define NUM_NON_TERMINALS 52
+#define NUM_NON_TERMINALS 53
 
-const char* NonTerminalID[] = {
+const char* NonTerminalIDs[] = {
     "program",
+    "otherFunctions",
     "mainFunction",
-	"otherFunctions",
-	"function",
-	"input_par",
-	"output_par",
-	"parameter_list",
-	"dataType",
-	"primitiveDatatype",
-	"constructedDatatype",
-	"remaining_list",
-	"stmts",
-	"typeDefinitions",
-	"typeDefinition",
-	"fieldDefinitions",
-	"fieldDefinition",
-	"moreFields",
-	"declarations",
-	"declaration",
-	"global_or_not",
-	"otherStmts",
-	"stmt",
-	"assignmentStmt",
-	"singleOrRecId",
-    "new_24",
-	"funCallStmt",
-	"outputParameters",
-	"inputParameters",
-	"iterativeStmt",
-	"conditionalStmt",
-	"elsePart",
-	"ioStmt",
-	"arithmeticExpression",
-	"expPrime",
-	"term",
-	"termPrime",
-	"factor",
-	"highPrecedenceOperators",
-	"lowPrecedenceOperators",
-    "all",
-	"temp",
+    "stmts",
+    "function",
+    "input_par",
+    "parameter_list",
+    "output_par",
+    "dataType",
+    "remaining_list",
+    "primitiveDatatype",
+    "constructiveDatatype",
+    "typeDefinitions",
+    "declarations",
+    "declaration",
+    "otherStmts",
+    "returnStmt",
+    "actualOrRedefined",
+    "typeDefinition",
+    "definetypestmt",
+    "fieldDefinitions",
+    "fieldDefinition",
+    "moreFields",
+    "fieldType",
+    "global_or_not",
+    "stmt",
+    "assignmentStmt",
+    "iterativeStmt",
+    "conditionalStmt",
+    "ioStmt",
+    "funCallStmt",
+    "singleOrRecId",
+    "arithmeticExpression",
+    "constructedVariable",
+    "oneExpansion",
+    "moreExpansions",
+    "outputParameters",
+    "inputParameters",
+    "idList",
     "booleanExpression",
+    "elsecondition",
     "var",
-	"logicalOp",
-	"relationalOp",
-	"returnStmt",
-	"optionalReturn",
-	"idList",
-	"more_ids"
+    "term",
+    "expPrime",
+    "lowPrecedenceOperators",
+    "factor",
+    "termPrime",
+    "highPrecedenceOperators",
+    "logicalOp",
+    "relationalOp",
+    "optionalReturn",
+    "more_ids",
+    "a"
 };
 
 
@@ -68,13 +72,13 @@ typedef struct Symbol {
 // LinkedList of symbols which is used for many purposes.
 typedef struct SymbolLinkedList {
     Symbol symbol; 
-    struct LinkedList* next; 
+    struct SymbolLinkedList* next; 
 } SymbolLinkedList;
 
 // Structure to represent the right hand side of grammar rule.
 typedef struct GrammarRule {
     int isEpsilon; // For determing wether the RHS of the grammar rule is epsilon or not.
-    SymbolLinkedList* RightHandSide; // stores the right hand side of the rule which can be a mixture of both terminals and non-Terminals
+    SymbolLinkedList* rightHandSide; // stores the right hand side of the rule which can be a mixture of both terminals and non-Terminals
 } GrammarRule;
 
 // Structure which encapsulates all the prodcution rules for a non-terminal
@@ -85,18 +89,16 @@ typedef struct ProductionRules {
     int maxSize;
 } ProductionRules;
 
-// Structure which encapsualtes all the rules of each non-termial 
+// Structure which encapsualtes all the rules of every non-termial 
 typedef struct Grammar {
     ProductionRules* productionRules;
     int numOfNonterminals;
 } Grammar;
 
 // This structure stores the first and follow sets for each non-terminal
-typedef struct FirstAndFollowSets {
+typedef struct FirstAndFollow {
     unsigned int** firstSets; // 2D array which contains the first sets of each non-terminal
     unsigned int** followSets; // 2D array which contains the follow sets of each non-terminal
 } FirstAndFollowSets;
-
-typedef struct
 
 #endif
